7f3e9677348d46ae8571a79d4d333538a248ac84
Hwangseonjun <seonjun97@naver.com>
2023-11-15 17:49:08 +0900

    1115 수업

---
+++ Step17_JDBC/src/test/main/MainClass14.java
@@ -0,0 +1,17 @@
+package test.main;
+
+import test.dao.MemberDao;
+
+public class MainClass14 {
+	public static void main(String[] args) {
+		//10 번 회원을 삭제 하려면?
+		int num=1;
+		//MemberDao 객체를 이용해서 삭제하기
+		boolean isSuccess=new MemberDao().delete(num);
+		if(isSuccess==true) {
+		System.out.println(num+"번 회원을 삭제 했습니다");
+		}else {
+			System.out.println(num+"번 회원은 없는 회원입니다.");
+		}
+	}
+}
