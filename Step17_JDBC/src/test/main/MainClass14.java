59875353fe7ddb43a783e688000202967e55473e
Hwangseonjun <seonjun97@naver.com>
2023-11-15 17:49:35 +0900

    1115 수업 추가

--- Step17_JDBC/src/test/main/MainClass14.java
+++ Step17_JDBC/src/test/main/MainClass14.java
@@ -8,7 +8,7 @@ public class MainClass14 {
 		int num=1;
 		//MemberDao 객체를 이용해서 삭제하기
 		boolean isSuccess=new MemberDao().delete(num);
-		if(isSuccess==true) {
+		if(isSuccess) {
 		System.out.println(num+"번 회원을 삭제 했습니다");
 		}else {
 			System.out.println(num+"번 회원은 없는 회원입니다.");
